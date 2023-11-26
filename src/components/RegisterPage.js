5232a8f9a1143e5ee4ffe0993798ff17151fba7b
Aleksander <aleksander.pawlak@op.pl>
2023-11-26 12:32:12 +0100

    register styles css module

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import '../styles/RegisterPage.css'
+import RegisterStyles from '../styles/RegisterPage.module.css'
 
 const RegisterPage = () => {
     return (
         <>
-            <div className="container">
-                <div className="login-box">
+            <div className={RegisterStyles.container}>
+                <div className={RegisterStyles['login-box']}>
                     <form action="">
                         <label for="email">Adres email:</label>
                         <input type="email" placeholder="Email"></input>
@@ -15,8 +15,8 @@ const RegisterPage = () => {
                         <input type="password" placeholder="Hasło"></input>
                         <label for="confirmPassword">Powtórz hasło:</label>
                         <input type="password" placeholder="Powtórz hasło"></input>
-                        <button type="submit" className="register">Zarejestruj</button>
-                        <button className="logIn">Zaloguj się</button>
+                        <button type="submit" className={RegisterStyles.register}>Zarejestruj</button>
+                        <button className={RegisterStyles.logIn}>Zaloguj się</button>
                     </form>
                 </div>
             </div>
