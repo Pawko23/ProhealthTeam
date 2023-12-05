419e6a2f5978610a773c4b3ee850621c35edba77
Aleksander <aleksander.pawlak@op.pl>
2023-12-05 18:11:43 +0100

    Created specific file for routes

---
+++ src/Routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RegisterPage from './components/RegisterPage';
+import LoginPage from './components/LoginPage';
+import TrainingMain from './components/TrainingMain';
+import DietMain from './components/DietMain';
+import TrainingChosen from './components/TrainingChosen';
+import RehabMain from './components/RehabMain';
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/training' element={<TrainingMain />} />
+      <Route path='/diet' element={<DietMain />} />
+      <Route path='/trainingchosen' element={<TrainingChosen />} />
+      <Route path='/rehabilitation' element={<RehabMain />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
