17a862119abeaa984866b24b21a060a949c4fff7
Aleksander <aleksander.pawlak@op.pl>
2024-01-03 13:03:30 +0100

    Showing results and matching color

--- src/components/BmiCalculator.js
+++ src/components/BmiCalculator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import Header from './Header';
 import Footer from './Footer';
@@ -7,10 +7,13 @@ import DietPageHero from '../img/diet.jpg';
 import CalcStyles from '../styles/CalculatorStyles.module.css';
 
 const BmiCalculator = () => {
-    const [age, setAge] = useState(1);
-    const [height, setHeight] = useState(1);
-    const [weight, setWeight] = useState(1);
-    const [bmi, setBmi] = useState(null);
+    const [age, setAge] = useState('');
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+    const [bmi, setBmi] = useState('');
+    const [calculatorHeight, setCalculatorHeight] = useState('75%');
+    const [resultColor, setResultColor] = useState('')
+    const [bmiMessage, setBmiMessage] = useState('')
 
     const handleAge = (event) => {
         setAge(event.target.value);
@@ -24,12 +27,52 @@ const BmiCalculator = () => {
         setWeight(event.target.value);
     }
 
+    useEffect(() => {
+      if(bmi!=null){
+        if(bmi < 16) {
+          setResultColor('#FF0000')
+          setBmiMessage('Wygłodzenie')
+        } 
+        else if(bmi >= 16 && bmi < 17) {
+          setResultColor('#FF9A00')
+          setBmiMessage('Wychudzenie')
+        }
+        else if(bmi >= 17 && bmi < 18.5) {
+          setResultColor('#E5C10D')
+          setBmiMessage('Niedowaga')
+        }
+        else if(bmi >= 18.5 && bmi < 25) {
+          setResultColor('#2BFF00')
+          setBmiMessage('Waga prawidłowa')
+        }
+        else if(bmi >= 25 && bmi < 30) {
+          setResultColor('#E5C10D')
+          setBmiMessage('Nadwaga')
+        }
+        else if(bmi >= 30 && bmi < 35) {
+          setResultColor('#FF9A00')
+          setBmiMessage('I stopień otyłości')
+        }
+        else if(bmi >= 35 && bmi < 40) {
+          setResultColor('#FF0000')
+          setBmiMessage('II stopień otyłości')
+        }
+        else if(bmi > 40) {
+          setResultColor('#FF0000')
+          setBmiMessage('Otyłość skrajna')
+        }
+      }
+    }, [bmi])
+
+
     const handleSubmit = (event) => {
         event.preventDefault()
 
         const heightToMeters = height / 100;
         const bmiResult = weight / (heightToMeters * heightToMeters);
         setBmi(bmiResult);
+
+        setCalculatorHeight('80%');
         console.log("Twoje BMI wynosi: ", bmiResult);
     }
 
@@ -41,7 +84,7 @@ const BmiCalculator = () => {
         heroImage={DietPageHero}
         title={'Kalkulator BMI'} />
       <div className={CalcStyles['calculator-container']}>
-        <div className={CalcStyles['calculator-box']}>
+        <div className={CalcStyles['calculator-box']} style={{ height: calculatorHeight}}>
           <h3>Kalkulator BMI</h3>
           <form onSubmit={handleSubmit}>
             <label>Wiek: </label>
@@ -51,6 +94,12 @@ const BmiCalculator = () => {
             <label>Waga: </label>
             <input type='number' value={weight} onChange={handleWeight}></input>
             <button type='submit'>Oblicz</button>
+            {bmi && (
+              <div className={CalcStyles['result-box']} style={{backgroundColor: resultColor}}>
+                <p>Result: {bmi}</p>
+                <p>{bmiMessage}</p>
+              </div>
+            )}
           </form>
         </div>
       </div>
