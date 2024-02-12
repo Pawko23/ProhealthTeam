ec08f8c752b7930f773d136b53057ad3e5840f08
Aleksander <aleksander.pawlak@op.pl>
2024-02-12 16:27:11 +0100

    initial model

--- src/components/RehabMain.js
+++ src/components/RehabMain.js
@@ -1,12 +1,20 @@
-import React from "react";
-import RehabStyles from '../styles/RehabMain.module.css'
+import React, { useState, useEffect, useRef } from "react";
+import styles from '../styles/RehabMain.module.css'
 import TrainingChosenHero from '../img/training-hero.jpg'
+import HumanModel from '../img/bones-muscles.png'
 import Navbar from "./Navbar";
 import Header from "./Header";
 import Footer from "./Footer";
 
 
 const RehabMain = () => {
+
+    const [fillColor, setFillColor] = useState('red')
+
+    const handleClick = () => {
+        setFillColor('green')
+    }
+
     return (
         <>
             <Navbar />
@@ -14,11 +22,12 @@ const RehabMain = () => {
                 heroImage={TrainingChosenHero}
                 title={'Rehabilitacja'}
             />
-            <div className={RehabStyles.container}>
-                <div className={RehabStyles['human-box']}></div>
-                <div className={RehabStyles['muscle-info']}></div>
-            </div>
-            <div className={RehabStyles['professional-box']}></div>
+            <section className={styles['model-container']}>
+                <svg width="400" height="600" viewBox="0 0 400 600">
+                    <image href={HumanModel} x="0" y="0" width="400" height="600" className={styles['human-model']}></image>
+                    <rect id="muscle1" x="15" y="0" width="20" height="20" fill={fillColor} onClick={handleClick} />
+                </svg>
+            </section>
             <Footer />
         </>
     )
