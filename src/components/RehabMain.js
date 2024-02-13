23d397ae9d57795d4658d8157ddb826c5dc7dd03
Aleksander <aleksander.pawlak@op.pl>
2024-02-13 11:34:16 +0100

    Lines toggling

--- src/components/RehabMain.js
+++ src/components/RehabMain.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import styles from '../styles/RehabMain.module.css'
 import TrainingChosenHero from '../img/training-hero.jpg'
 import HumanModel from '../img/bones-muscles.png'
@@ -9,11 +9,11 @@ import Footer from "./Footer";
 
 const RehabMain = () => {
 
-    const [fillColor, setFillColor] = useState('transparent')
-    const [lineCoordinates, setLineCoordinates] = useState(null)
-    const [pathLine, setPathLine] = useState(null)
+    const [fillColor, setFillColor] = useState('red')
+    const [lineCoordinates, setLineCoordinates] = useState([])
+    // const [pathLine, setPathLine] = useState(null)
 
-    const handleClick = (event) => {
+    const handleClick = (event, muscle) => {
         const circle = event.target
         const bbox = circle.getBBox()
         const centerX = bbox.x + bbox.width / 2
@@ -22,9 +22,17 @@ const RehabMain = () => {
         const endY = centerY
         const controlPointX = centerX + 50; // x coordinate for the control point (halfway between start and end)
         const controlPointY = centerY - 50; // y coordinate for the control point (45 degrees up)
-        setLineCoordinates({ startX: centerX, startY: centerY, endX, endY })
-        setPathLine(`M ${centerX},${centerY} L ${controlPointX},${controlPointY} L ${endX}, ${endY}`)
-        // setFillColor('green')
+
+        const existingIndex = lineCoordinates.findIndex(coord => coord.muscle === muscle)
+        if(existingIndex !== -1) {
+            setLineCoordinates(prevCoordinates => prevCoordinates.filter((_, index) => index!== existingIndex))
+        } else {
+            setLineCoordinates(prevCoordinates => [...prevCoordinates, { startX: centerX, startY: centerY, endX, endY, muscle }])
+        }
+        
+        // setPathLine(`M ${centerX},${centerY} L ${controlPointX},${controlPointY} L ${endX}, ${endY}`)
+        console.log(muscle);
+        setFillColor('green')
     }
 
     return (
@@ -37,16 +45,18 @@ const RehabMain = () => {
             <section className={styles['model-container']}>
                 <svg width="400" height="600" viewBox="0 0 400 600">
                     <image href={HumanModel} x="0" y="0" width="400" height="600" className={styles['human-model']}></image>
-                    <circle id="muscle1" cx="255" cy="115" r="7" fill={fillColor} onClick={handleClick} />
-                    {lineCoordinates && (
-                        <line 
-                            x1={lineCoordinates.startX}
-                            y1={lineCoordinates.startY}
-                            x2={lineCoordinates.endX}
-                            y2={lineCoordinates.endY}
-                            stroke="black"
-                        />
-                    )}
+                    <circle id="naramienny" cx="255" cy="115" r="7" fill={fillColor} onClick={(event) => handleClick(event, 'naramienny')} />
+                    <circle id="naramienny" cx="260" cy="160" r="7" fill={fillColor} onClick={(event) => handleClick(event, 'biceps')} />
+                    {lineCoordinates.map((coords, index) => (
+                            <line 
+                                key={index}
+                                x1={coords.startX}
+                                y1={coords.startY}
+                                x2={coords.endX}
+                                y2={coords.endY}
+                                stroke="black"
+                            />
+                    ))}
                 </svg>
             </section>
             <Footer />
