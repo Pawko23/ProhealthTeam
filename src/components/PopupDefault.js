73b6c6627846dd7d973c91cda4abb3484dd381dd
Aleksander <aleksander.pawlak@op.pl>
2024-02-15 18:12:48 +0100

    Popup fixes

--- src/components/PopupDefault.js
+++ src/components/PopupDefault.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from '../styles/PopupDefaultStyles.module.css'
 
 const PopupDefault = ( {info, onClose} ) => {
-    console.log(info)
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, []);
+
+    const handleBackdropClick = (event) => {
+        console.log('Backdrop')
+        if (event.target.classList.contains(styles['popup-container'])) {
+            onClose();
+        }
+    };
+
     const handleConfirm = () => {
         onClose()
     }
     
     return (
         <>
-            <div className={styles['popup-container']}>
+            <div className={styles['popup-container']} onClick={handleBackdropClick}>
                 <div className={styles['popup-content']}>
                     <p> {info} </p>
+                    <button onClick={handleConfirm}>Ok</button>
                 </div>
-                <button onClick={handleConfirm}>Potwierdź</button>
             </div>
         </>
     )
