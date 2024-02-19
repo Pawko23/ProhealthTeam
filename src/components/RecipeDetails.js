1bbc510868aae37148eadc2857cc767cea27d7b4
Aleksander <aleksander.pawlak@op.pl>
2024-02-19 16:26:00 +0100

    diet main and recipe details

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -41,32 +41,40 @@ const RecipeDetails = () => {
     <>
       <Navbar />
       <div className={RecipeStyles.container}>
-
-        <div className={RecipeStyles['recipe-info']}>
-          <p className={RecipeStyles['recipe-name']}>{recipe.name}</p>
-          <p className={RecipeStyles['recipe-level']}>{recipe.difficulty}</p>
+        <div className={RecipeDetails['grid-box1']}>
+          <div className={RecipeStyles['recipe-image']}>
+            <img src={`/${recipe.image}`} alt={recipe.name}/>
+          </div>
+          <div className={RecipeStyles['recipe-info']}>
+            <p className={RecipeStyles['recipe-name']}>Nazwa: <span>{recipe.name}</span></p>
+            <p className={RecipeStyles['recipe-level']}>Poziom trudnośći: <span>{recipe.difficulty}</span></p>
+          </div>
+        </div>
+        <div className={RecipeStyles['grid-box2']}>
+          <div className={RecipeStyles['portion-info']}>W 100g:</div>
+          <div className={RecipeStyles['macros-box']}>
+            <div className={classNames(RecipeStyles.kcal, RecipeStyles.box)}>{recipe.kcal} Kcal</div>
+            <div className={classNames(RecipeStyles.protein, RecipeStyles.box)}>{recipe.protein} Białka</div>
+            <div className={classNames(RecipeStyles.fats, RecipeStyles.box)}>{recipe.fats} Tłuszczy</div>
+            <div className={classNames(RecipeStyles.carbs, RecipeStyles.box)}>{recipe.carbs} Węglowodanów</div>
+          </div>
         </div>
 
-        <div className={RecipeStyles['recipe-image']}>
-          <img src={`/${recipe.image}`} alt={recipe.name}/>
-        </div>
-        <div className={RecipeStyles['macros-box']}>
-          <div className={classNames(RecipeStyles.kcal, RecipeStyles.box)}>{recipe.kcal} Kcal</div>
-          <div className={classNames(RecipeStyles.protein, RecipeStyles.box)}>{recipe.protein} Białka</div>
-          <div className={classNames(RecipeStyles.fats, RecipeStyles.box)}>{recipe.fats} Tłuszczy</div>
-          <div className={classNames(RecipeStyles.carbs, RecipeStyles.box)}>{recipe.carbs} Węglowodanów</div>
-        </div>
-
-        <div className={RecipeStyles['ingredients-box']}>
-          <h3>Składniki</h3>
-            {recipe.ingredients.map((ingredient) => (
-              <li>{ingredient}</li>
-            ))}
+        <div className={RecipeStyles['grid-box3']}>
+          <div className={RecipeStyles['ingredients-box']}>
+            <h3>Składniki</h3>
+              {recipe.ingredients.map((ingredient) => (
+                <li>{ingredient}</li>
+              ))}
+          </div>
         </div>
 
-        <div className={RecipeStyles['prepare-box']}>
-          <p className={RecipeStyles.description}>{recipe.description}
-          </p>
+        <div className={RecipeStyles['grid-box4']}>
+          <div className={RecipeStyles['prepare-box']}>
+            <h3>Przygotowanie</h3>
+            <p className={RecipeStyles.description}>{recipe.description}
+            </p>
+          </div>
         </div>
       </div>
       <Footer />
